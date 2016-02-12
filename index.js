f4db9eb8c4e1ba4609f8a1ae9e8dd512fb8eed19
Anil Kumar <anil.akbp@gmail.com>
2016-02-12 02:54:44 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,145 @@
+#! /usr/bin/env node
+
+
+(function() {
+
+
+// Require RCSDK
+var RCSDK = require('rcsdk');
+var fs = require('fs');
+var extensions = [];
+var activecalls = [];
+var rcsdk = new RCSDK({
+	server: 'https://platform.devtest.ringcentral.com', // https://platform.ringcentral.com ( Production )
+	appKey: 'yourAppSecret',
+	appSecret: 'yourAppKey'
+});
+
+
+//Platform Singleton
+var platform = rcsdk.getPlatform();
+
+
+// Active calls function
+var activeCalls = function() {
+    platform.get('/account/~/active-calls', {
+            query: {
+                direction: "Outbound",  // Direction
+                type: "Voice"          // Type of call
+            }
+        })
+        .then(function(response){
+            activecalls = response.json.records;
+        })
+        .catch(function(e){
+            console.log('Error inside Subscription event for active calls :' + e.stack);
+        });
+}
+
+
+// Authenticate
+platform.authorize({
+	username: 'Your Username',            // Enter the usernmae
+	extension: 'your Extension',          // Enter the extension
+	password: 'your password',            // Enter the password
+	remember: 'true'
+}).then(function(response){
+  
+    platform.auth = response.json;
+    console.log('Authentication success');
+    console.log(JSON.stringify(response.data, null, 2));
+
+
+        //********** Retreive Account Active calls numbers from the extension ********************
+        //     //ret  reive all the numbers associated with this extension
+
+        platform.get('/account/~/extension')
+            .then(function(response){
+
+                var apiresponse = response.json;
+                console.log("********************Phone Numbers for the Extension*********************");
+                console.log(JSON.stringify(response.data, null, 2));
+
+            })
+            .catch(function(e){
+                console.error('Error ' + e.stack);
+            });
+
+     //********** Retreive Phone numbers from the extension ********************
+         //ret  reive all the numbers associated with this extension
+    platform.get('/account/~/device')
+        .then(function(response){
+
+            var apiresponse = response.json;
+            console.log("**************** Extensions Device List  ***********************");
+            console.log(JSON.stringify(response.data, null, 2));
+
+        })
+        .catch(function(e){
+            console.error('Error ' + e.stack);
+        });
+
+
+        //********** Setup Subscriptions ********************
+        //retreive all the extensions for this account
+        platform.get('/account/~/extension/')
+                 .then(function(response){
+                     console.log(JSON.stringify(response.data, null, 2));
+                   var apiresponse = response.json;
+                   for(var key in apiresponse.records) {
+                     var extension_number = "";
+
+                     if (apiresponse.records[key].hasOwnProperty('extensionNumber')) {
+                         extension_number = parseInt(apiresponse.records[key].id);
+                         extensions.push(['/account/~/extension/' + extension_number + '/presence']);
+                         extensions.push(['/account/~/extension/' + extension_number + '/message-store']);
+                     }
+                 }
+
+                 // Keep pulling the active calls
+                     setInterval(activeCalls(), 6000);
+
+                 // Create a subscription
+                     var subscription = rcsdk.getSubscription();
+                     console.log("*************** Subscription: *****************", subscription);
+
+                     subscription.setEvents(extensions);
+
+                     subscription.on(subscription.events.notification, function(msg) {
+                         console.log("A new Event");
+                         console.log("***********");
+                         console.log(JSON.stringify(msg));
+
+                         if(msg.body.telephonyStatus == "CallConnected") {
+
+                              for(var key in records.records) {
+                                  if(records[key].result == "Call connected") {
+                                      if(records[key].to.phoneNumber == "511") {
+                                          console.log("511 identified");
+                                      }
+                                  }
+                              }
+
+                         }
+
+                     });
+
+                     return subscription.register();
+
+               })
+               .then(function(response) {
+                     console.log('Subscription success');
+                     console.log(JSON.stringify(response.data, null, 2));;
+               })
+               .catch(function(e){
+                 console.error('Error ' + e.stack);
+               });
+
+
+   })
+   .catch(function(e){
+    console.error('Error ' + e.stack);
+  });
+
+
+  }) ();       
---
+++ node_modules/rcsdk/bower_components/es6-promise-polyfill/CHANGELOG.md
@@ -0,0 +1,13 @@
+## 1.1.0 (October 16, 2015)
+
+- Change way to get reference to `global`, since `Function()()` may be prohibited
+- Add AMD support
+
+## 1.0.1 (September 14, 2015)
+
+- Fix exports - use polyfill only if native implementation isn't supported
+- Add license
+
+## 1.0.0 (June 19, 2014)
+
+- Init release
